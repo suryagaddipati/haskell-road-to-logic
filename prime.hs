350a8590df481703aa9564beaf7e48eafcaabff3
Surya Gaddipati <sgaddipati@groupon.com>
2014-01-02 19:14:59 -0600

    prime take 1

--- prime.hs
+++ prime.hs
@@ -1,1 +1,14 @@
 divides d n = rem n d == 0
+
+ldf k n | divides k n = k
+        | k^2 > n = n -- if k^2 >= n then n is not prime => k^2 < n => n is prime
+        | otherwise = ldf (k+1) n
+
+ld n = ldf 2 n
+
+prime n | n < 1 = error " should be greater than 1"
+        | n == 1 = False
+        | otherwise = ld n == n 
+
+
+
